1db8ccac08329a2e8168bda22c2a4ff5221a3318
Kabimbus <kabimbus13@gmail.com>
2021-04-10 14:34:20 -0500

    styling beta (button ex.)

--- VendorFriendFrontend/VendorFriendFrontend/ClientApp/theme.js
+++ VendorFriendFrontend/VendorFriendFrontend/ClientApp/theme.js
@@ -9,3 +9,34 @@ export default createMuiTheme({
     secondary: indigo // Indigo is probably a good match with pink
   }
 });
+
+export default buttonStyling = {
+  background: {
+    primary: "blue",
+    secondary: "white",
+    other: "grey",
+  },
+  typography: {
+    colorVariants: {
+      primary: "#000000",
+      secondary: "#FFFFFF",
+    },
+    fontFamily: {
+      primary: "Times New Roman",
+      secondary: "Arial Black"
+    }
+  },
+  icons: {
+    endicon: {
+      secondary: <Avatar src={'https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Ic_shopping_cart_48px.svg/768px-Ic_shopping_cart_48px.svg.png'}></Avatar>,
+    }
+  },
+  bordering: {
+    primary: contained,
+    secondary: outlined
+  },
+};
+
+export default actualBackground = {
+
+}
