3542982a320caa86eaac8961c5b749a9f64efb3a
Timm-Tomm <61246550+Timm-Tomm@users.noreply.github.com>
2021-04-10 12:42:28 -0500

    Add universal background theme

--- VendorFriendFrontend/VendorFriendFrontend/ClientApp/src/App.js
+++ VendorFriendFrontend/VendorFriendFrontend/ClientApp/src/App.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import Home from './components/Home';
+import Attendee from './components/search';
+import { createMuiTheme, makeStyles, MuiThemeProvider, withStyles} from '@material-ui/core/styles';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 import './custom.css'
 
@@ -11,12 +14,23 @@ export default class App extends Component {
   static displayName = App.name;
 
   render () {
+    const theme = createMuiTheme({
+      palette: {
+          background: {
+              default: "LightBlue"
+          }
+      }
+    });
     return (
-      <Layout>
+      <MuiThemeProvider theme={theme}>
+            <CssBaseline />
+            <Layout>
         <Route exact path='/' component={Home} />
+        <Route exact path='/search' component={Attendee} />
         <Route path='/counter' component={Counter} />
         <Route path='/fetch-data' component={FetchData} />
-      </Layout>
+        </Layout>
+      </MuiThemeProvider>
     );
   }
 }
